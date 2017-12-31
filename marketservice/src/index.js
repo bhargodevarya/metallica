cb15f5a3c921afa27a6442a8ff473995e479522f
Bhargodev Arya <barya@sapient.com>
2017-12-31 22:58:27 +0530

    marketservice repo setup, publishing metal prices on rabbitmq

---
+++ marketservice/src/index.js
@@ -0,0 +1,28 @@
+const MetalInfo = require('./schema/MetalInfo')
+const qClient = require('../../rabbitmqclient/src/index')
+
+let AU = new MetalInfo('AU', 100)
+let AL = new MetalInfo('AL', 100)
+let AG = new MetalInfo('AG', 100)
+let CU = new MetalInfo('CU', 100)
+let ZN = new MetalInfo('ZN', 100)
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+setInterval(() => {
+    AU.price=getRandomInt(1000,2000)
+    AG.price=getRandomInt(1000,2000)
+    AL.price=getRandomInt(1000,2000)
+    CU.price=getRandomInt(1000,2000)
+    ZN.price=getRandomInt(1000,2000)
+    console.log(AU,AG,AL,CU,ZN)
+    qClient.publishMessage('market.ops','data.au',AU)
+    qClient.publishMessage('market.ops','data.ag',AG)
+    qClient.publishMessage('market.ops','data.al',AL)
+    qClient.publishMessage('market.ops','data.cu',CU)
+    qClient.publishMessage('market.ops','data.zn',ZN)
+},5000)
---
+++ marketservice/src/schema/MetalInfo.js
@@ -0,0 +1,8 @@
+class MetalInfo {
+    constructor(commodity, price) {
+        this.commodity = commodity
+        this.price = price
+    }
+}
+
+module.exports = MetalInfo
